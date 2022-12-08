1cfcbcb6f83cda4d083a22e9c2ea493989ee6d2a
kaelma <kaelma@tencent.com>
2022-12-08 23:30:20 +0800

    fix bug about lerp key rots part1 - task2

--- lab2/answer_task1.py
+++ lab2/answer_task1.py
@@ -302,14 +302,16 @@ def blend_two_motions(bvh_motion1, bvh_motion2, alpha):
     for i in range(res.joint_rotation.shape[1]):
         key_times.append(2*i)
         key_times.append(2*i + 1)
-    times = [alpha[i] + i for i in range(res.joint_rotation.shape[1])]
+    times = [alpha[i] + 2 * i for i in range(res.joint_rotation.shape[1])]
 
     for i in range(frames):
         j = np.around(bvh_motion1.joint_position.shape[0] / frames * i).astype(np.int32)
         k = np.around(bvh_motion2.joint_position.shape[0] / frames * i).astype(np.int32)
         res.joint_position[i] = (1 - alpha[i]) * bvh_motion1.joint_position[j] + alpha[i] * bvh_motion2.joint_position[k]
 
-        rotations = np.concatenate([bvh_motion1.joint_rotation[j], bvh_motion2.joint_rotation[k]])
+        key_rots = [[bvh_motion1.joint_rotation[j][p], bvh_motion2.joint_rotation[k][p]]
+                    for p in range(res.joint_rotation.shape[1])]
+        rotations = np.concatenate(key_rots)
         slerp = Slerp(key_times, R.from_quat(rotations))
         res.joint_rotation[i] = slerp(times).as_quat()
 
